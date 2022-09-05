739dd0d13ba6f19ccdf3fb6eb9bddfc97152a971
Parash <85245101+parashie@users.noreply.github.com>
2022-09-04 20:25:55 -0400

    Update caloricDeficitCalculator.py

--- caloricDeficitCalculator.py
+++ caloricDeficitCalculator.py
@@ -1,6 +1,6 @@
 import math
 
-# this calculator is not meant to replace a dietian, doctor, or nutritionist it is purely for calculation purposes
+# this calculator is not meant to replace a dietician, doctor, or nutritionist it is purely for calculation purposes
 # This calculator takes the users weight in pounds, goal in pounds, subtracts the two and converts the difference into calories
 # calculate the calories needed to lose weight per day
 
